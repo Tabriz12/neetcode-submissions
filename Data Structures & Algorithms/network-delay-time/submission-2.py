5f951810de5cf2d5a6af2d40402873ae89d5fea6
Tabriz Nuruyev <38668732+Tabriz12@users.noreply.github.com>
2026-08-22 13:53:43 +0200

    Add: network-delay-time - submission-2

---
+++ Data Structures & Algorithms/network-delay-time/submission-2.py
@@ -0,0 +1,94 @@
+from heapq import heapify, heappop, heappush
+class Solution:
+
+
+    def dijkstra(self, times, n, k):
+
+        dst = [float('inf')] * (n+1)
+
+        dst[k] = 0
+
+        routes = [[] for _ in range(n+1)]
+
+        for s,d,t in times:
+
+            routes[s].append([d,t])
+        
+        h = [[0, k]]
+
+        heapify(h)
+
+
+        while h:
+
+            t, s = heappop(h)
+
+            for dest, dly in routes[s]:
+
+                if t+dly < dst[dest]:
+
+                    dst[dest] = t + dly
+                    heappush(h, [t+dly, dest])
+        
+        tot = max(dst[1:])
+
+        return -1 if tot == float('inf') else tot
+    
+
+    def dfsing(self, times, n, k):
+
+        dst = [float('inf')] * (n+1)
+
+        dst[k] = 0
+
+        routes = [[] for _ in range(n+1)]
+
+        for s,d,t in times:
+
+            routes[s].append([d,t])
+        
+
+        def dfs(src):
+
+            for d,t in routes[src]:
+
+                if dst[src] + t < dst[d]:
+
+                    dst[d] = dst[src] + t
+
+                    dfs(d)
+        
+        dfs(k)
+        
+        tot = max(dst[1:])
+
+        return -1 if tot == float('inf') else tot
+
+
+
+
+
+
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+
+        return self.dfsing(times, n, k)
+
+        
+            
+
+        
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+        
