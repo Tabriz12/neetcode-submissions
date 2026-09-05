3ad61768cdf94c66f658720dffea98e126f74ed6
TabrizN <38668732+Tabriz12@users.noreply.github.com>
2026-09-05 22:20:45 +0200

    Add: factory - submission-0

---
+++ Design Patterns/factory/submission-0.py
@@ -0,0 +1,45 @@
+class Vehicle(ABC):
+    @abstractmethod
+    def getType(self) -> str:
+        pass
+
+class Car(Vehicle):
+    def getType(self) -> str:
+        return "Car"
+
+class Bike(Vehicle):
+    def getType(self) -> str:
+        return "Bike"
+
+class Truck(Vehicle):
+    def getType(self) -> str:
+        return "Truck"
+
+class VehicleFactory(ABC):
+    @abstractmethod
+    def createVehicle(self) -> Vehicle:
+        pass
+
+class CarFactory(VehicleFactory):
+    
+    def createVehicle(self):
+
+        car = Car()
+
+        return car
+
+class BikeFactory(VehicleFactory):
+    
+    def createVehicle(self):
+
+        bike  = Bike()
+
+        return bike
+
+class TruckFactory(VehicleFactory):
+
+    def createVehicle(self):
+
+        truck  = Truck()
+
+        return truck
